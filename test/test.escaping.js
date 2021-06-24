6826b1805df0d55d8c6d189759dd02ee43dc5206
Brett Zamir <brettz9@yahoo.com>
2021-06-24 10:24:38 +0800

    INCOMPLETE: even adding the at-sign (haven't yet tried referencing it, e.g., with `@, gives a max. call stack size exceeded error

--- test/test.escaping.js
+++ test/test.escaping.js
@@ -1,17 +1,59 @@
 
 describe('JSONPath - Escaping', function () {
-    const json = {
-        '*': 'star',
-        'rest': 'rest',
-        'foo': 'bar'
-    };
-
-    const jsonMissingSpecial = {
-        'rest': 'rest',
-        'foo': 'bar'
-    };
-
     it('escape *', () => {
+        const json = {
+            '*': 'star',
+            'rest': 'rest',
+            'foo': 'bar'
+        };
+
+        const jsonMissingSpecial = {
+            'rest': 'rest',
+            'foo': 'bar'
+        };
+
+        let expected = ['star'];
+        let result = jsonpath({json, path: "$['`*']"});
+        assert.deepEqual(result, expected);
+
+        expected = [];
+        result = jsonpath({json: jsonMissingSpecial, path: "$['`*']"});
+        assert.deepEqual(result, expected);
+
+        expected = ['star', 'rest'];
+        result = jsonpath({json, path: "$[`*,rest]"});
+        assert.deepEqual(result, expected);
+
+        expected = ['star'];
+        result = jsonpath({json, path: "$.`*"});
+        assert.deepEqual(result, expected);
+
+        expected = [];
+        result = jsonpath({json: jsonMissingSpecial, path: "$.`*"});
+        assert.deepEqual(result, expected);
+
+        expected = ['star', 'rest', 'bar'];
+        result = jsonpath({json, path: "$['*']"});
+        assert.deepEqual(result, expected);
+
+        expected = ['rest', 'bar'];
+        result = jsonpath({json: jsonMissingSpecial, path: "$['*']"});
+        assert.deepEqual(result, expected);
+    });
+
+    it.only('escape *', () => {
+        const json = {
+            '*': 'star',
+            '@': 'something',
+            'rest': 'rest',
+            'foo': 'bar'
+        };
+
+        const jsonMissingSpecial = {
+            'rest': 'rest',
+            'foo': 'bar'
+        };
+
         let expected = ['star'];
         let result = jsonpath({json, path: "$['`*']"});
         assert.deepEqual(result, expected);
